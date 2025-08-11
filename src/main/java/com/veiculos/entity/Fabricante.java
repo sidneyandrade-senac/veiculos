47a013e65344a7bec797e7dd8f984f9d9e63f361
veiculos <sidney.andrade@prof.sc.senac.br>
2025-08-11 18:47:19 -0300

    Adição do atributo [paisOrigem] na classe Fabricante

--- src/main/java/com/veiculos/entity/Fabricante.java
+++ src/main/java/com/veiculos/entity/Fabricante.java
@@ -1,5 +1,6 @@
 package com.veiculos.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,13 +12,18 @@ public class Fabricante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true, length = 100)
     private String nome;
+
+    @Column(nullable = false, length = 50)
+    private String paisOrigem;
 
     public Fabricante() {
     }
 
-    public Fabricante(String nome) {
+    public Fabricante(String nome, String paisOrigem) {
         this.nome = nome;
+        this.paisOrigem = paisOrigem;
     }
 
     public Long getId() {
@@ -28,12 +34,20 @@ public class Fabricante {
         return nome;
     }
 
+    public String getPaisOrigem() {
+        return paisOrigem;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public void setPaisOrigem(String paisOrigem) {
+        this.paisOrigem = paisOrigem;
     }
 
 }
