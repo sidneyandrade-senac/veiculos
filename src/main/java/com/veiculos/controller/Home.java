3631c11cb35abcea9ecc2fec5fdc7fa13c8b9a36
veiculos <sidney.andrade@prof.sc.senac.br>
2025-08-20 16:46:45 -0300

    feat: (api/home) Adição de novo end-point na api home

--- src/main/java/com/veiculos/controller/Home.java
+++ src/main/java/com/veiculos/controller/Home.java
@@ -1,14 +1,26 @@
 package com.veiculos.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Home {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String index() {
-        return "Bem-vindo ao sistema de gerenciamento de veículos!";
+        return "Você esta na mapeamento inicial";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "você esta mapeamento HOME";
+    }
+
+    //http://localhost:8080/soma/4/7
+    @GetMapping("/soma/{num1}/{num2}")
+    public String soma(@PathVariable int num1, @PathVariable int num2) {
+        return "você esta mapeamento SOMA de " + num1 + " + " + num2 + "  = " + (num1 + num2);
     }
 
 }
